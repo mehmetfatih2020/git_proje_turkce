45389747bac9bffecabb376569c8d4104508eec8
Fatih <ozzenginmehmetfatihozzengin@gmail.com>
2020-06-26 09:00:34 +0300

    son conflicy

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,7 +6,7 @@ public class FirstClass {
 
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
-		System.out.println("ucuncu commit local repositoryden");
+		System.out.println("2. kez conflict");
 		System.out.println("conflict yapmak istiyoruz");
 
 		System.out.println("first commit from remote repository");
